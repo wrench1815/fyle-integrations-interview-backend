d4f803db72273a45cc6941a6e413fe012a7c4ee8
wrench1815 <hardeepkumar1815@gmail.com>
2022-10-14 13:26:36 +0530

    👌 IMPROVE: migrate to poetry from pip for dep management add yapf for formatting update and iclude models in admin include admin url

--- apps/students/admin.py
+++ apps/students/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Assignment, Student
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    '''Admin View for Assignment'''
+
+    list_display = [
+        'student',
+        'teacher',
+        'content',
+        'grade',
+    ]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    '''Admin View for Student'''
+
+    list_display = [
+        'user',
+        'created_at',
+    ]
